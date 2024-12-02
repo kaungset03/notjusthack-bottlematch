bd91cb4fb1f80017755faafca74795d04a3b2a20
Kaung Set <kset2299@gmail.com>
2024-12-02 21:14:43 +0630

    first commit

---
+++ app/game.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+const game = () => {
+  return (
+    <View>
+      <Text>game</Text>
+    </View>
+  );
+};
+export default game;
+const styles = StyleSheet.create({});
